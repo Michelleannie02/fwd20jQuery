3b1e87b9c57c8f3fc9f367f092b1a40856775f17
bjornwann <bjorn.wann@chasacademy.se>
2021-02-11 15:54:37 +0100

    jQuery del 2

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@ $(()=>{
     // const headline = $('h1');
     // headline.remove();
     // console.log(headline);
+    
     const main = $('#main');
     const pl2 = $('.pl-2');
-    console.log(main, pl2);
+    //console.log(main, pl2);
 
     let myShinyNewDiv = $('<div class="w-full md:w-1/2 xl:w-1/3 p-6">');
     myShinyNewDiv.html('<div class="bg-gradient-to-b from-green-200 to-green-100 border-b-4 border-green-600 rounded-lg shadow-xl p-5"><ul><li>item</li><li class="secret">item</li><li>item</li></ul>');
@@ -26,6 +27,62 @@ $(()=>{
     const firstListItem = $(listItems).first();
     const nextListItem = $(firstListItem).next();
     const prevListItem = $(nextListItem).prev();
-    console.log(prevListItem);
+    
 
+    const listChildren = $('#content ul li').children();
+    const listChildrenParent = $(listChildren).parent();
+    console.log(listChildrenParent);
+
+    $(document).on('click', function(event) {
+        $(event.target).closest('li').addClass('uppercase');
+    });
+
+    $('#main').on('mouseenter', function(){
+        $(this).css('color', 'red');
+    });
+
+    $('#main').on('mouseleave', function(){
+        $(this).css('color', 'blue');
+    });
+
+    const chuckText = $('#chuckdiv p');
+    const chuckButton = $('#chuckbutton');
+
+    chuckButton.on('click', function(){
+        $.ajax({
+            url: 'https://api.chucknorris.io/jokes/random',
+            contentType: 'application/json',
+            error: function(err) {
+                console.log('something went horribly wrong', err);
+            },
+            success: function(result){
+                chuckText.text(result.value);
+            }
+        });
+    });
+    
+
+
+    /*
+    uppgift
+    lägg in en knapp i HTML-filen som ni sedan selectar med jQuery
+    lägg in en eventhandler på knappen som kör en ajaxförfrågan till api:et och skriver ut svaret från result.value i ett annat html-element
+    */
+
+    $.widget('chas.testwidget', {
+        options: {
+            someValue: 0
+        },
+        _create:function() {
+            const someValue = `testing options: ${this.options.someValue}`
+            this.element
+            .addClass('testwidget')
+            .find('p')
+            .text(someValue);
+        }
+    });
+    console.log($.chas);
+    
+    $('#chuckdiv').testwidget();
+    
 });
